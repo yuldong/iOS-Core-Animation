9491b4adb73e8ae8bac8e7d35f09c5e645773636
yuldong <yuldong@126.com>
2020-04-03 22:25:22 +0800

    隐式动画的本质以及尝试

--- CAnimation/AnimationsViewController.swift
+++ CAnimation/AnimationsViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class AnimationsViewController: UIViewController {
+    
+    @IBOutlet weak var layerView: UIView!
+    
+    var colorLayer: CALayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colorLayer = CALayer()
+        colorLayer.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        colorLayer.backgroundColor = UIColor.blue.cgColor
+        layerView.layer.addSublayer(colorLayer)
+    }
+    
+    @IBAction func changerColor() {
+        CATransaction.begin()
+        CATransaction.setAnimationDuration(2.0)
+        CATransaction.setCompletionBlock {
+            var transform = self.colorLayer.affineTransform()
+            transform = transform.rotated(by: CGFloat.pi / 2)
+            self.colorLayer.setAffineTransform(transform)
+        }
+        let red = CGFloat(arc4random()) / CGFloat(Int32.max)
+        let green = CGFloat(arc4random()) / CGFloat(Int32.max)
+        let blue = CGFloat(arc4random()) / CGFloat(Int32.max)
+        colorLayer.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1).cgColor
+        CATransaction.commit()
     }
 
 }
