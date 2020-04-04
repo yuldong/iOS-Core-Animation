6d612d2069a236bf50e8af9e0bc09a14fc74dcad
yuldong <yuldong@126.com>
2020-04-04 19:14:56 +0800

    动画中的时间与缓冲函数

--- CAnimation/AnimationsViewController.swift
+++ CAnimation/AnimationsViewController.swift
@@ -14,46 +14,55 @@ class AnimationsViewController: UIViewController {
     
     @IBOutlet weak var imgView: UIImageView!
     
+    var ballImgView: UIImageView!
+    
     var colorLayer: CALayer!
     
     var shipLayer: CALayer!
     
     let imgs = [UIImage(named: "flower"), UIImage(named: "snowman"), UIImage(named: "ship")]
-
+    
+    let mediaTimeNames: [CAMediaTimingFunctionName] = [.easeIn, .easeInEaseOut, .easeOut, .linear, .default]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setUp()
-//        setUp2()
-//        setUpBasicCA()
-        shipLayer = CALayer()
-        shipLayer.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
-        shipLayer.position = CGPoint(x: 0, y: 150)
-        shipLayer.contents = UIImage(named: "ship")?.cgImage
-        shipLayer.contentsGravity = CALayerContentsGravity.resizeAspect
-        layerView.layer.addSublayer(shipLayer)
+        //        setUp()
+        //        setUp2()
+        //        setUpBasicCA()
+        //        shipLayer = CALayer()
+        //        shipLayer.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
+        //        shipLayer.position = CGPoint(x: 0, y: 150)
+        //        shipLayer.contents = UIImage(named: "ship")?.cgImage
+        //        shipLayer.contentsGravity = CALayerContentsGravity.resizeAspect
+        //        layerView.layer.addSublayer(shipLayer)
+        
+        ballImgView = UIImageView(image: UIImage(named: "ball"))
+        self.layerView.addSubview(ballImgView)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        let point = touches.first!.location(in: view)
-//        if (colorLayer.presentation()?.hitTest(point)) != nil {
-//            let red = CGFloat(arc4random()) / CGFloat(Int32.max)
-//            let green = CGFloat(arc4random()) / CGFloat(Int32.max)
-//            let blue = CGFloat(arc4random()) / CGFloat(Int32.max)
-//            colorLayer.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1).cgColor
-//        } else {
-//            CATransaction.begin()
-//            CATransaction.setAnimationDuration(4.0)
-//            colorLayer.position = point
-//            CATransaction.commit()
-//        }
+        //        let point = touches.first!.location(in: view)
+        //        if (colorLayer.presentation()?.hitTest(point)) != nil {
+        //            let red = CGFloat(arc4random()) / CGFloat(Int32.max)
+        //            let green = CGFloat(arc4random()) / CGFloat(Int32.max)
+        //            let blue = CGFloat(arc4random()) / CGFloat(Int32.max)
+        //            colorLayer.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1).cgColor
+        //        } else {
+        //            CATransaction.begin()
+        //            CATransaction.setAnimationDuration(4.0)
+        //            colorLayer.position = point
+        //            CATransaction.commit()
+        //        }
     }
     
     @IBAction func changerColor() {
-//        setUp_func()
-//        setUpBasicCA_func()
-//        setUpKeyCA()
-//        setUpCATransition()
-//        customTransition()
+        //        setUp_func()
+        //        setUpBasicCA_func()
+        //        setUpKeyCA()
+        //        setUpCATransition()
+        //        customTransition()
+        //        drawMediaFunction()
+        ballJumps()
     }
     
     @IBAction func start() {
@@ -100,39 +109,42 @@ class AnimationsViewController: UIViewController {
     }
     
     func setUpKeyCA() {
-//        let animation = CAKeyframeAnimation()
-//        animation.keyPath = "backgroundColor"
-//        animation.duration = 4.0
-//        // 需要开始和结束的关键帧来匹配当前属性的值
-//        animation.values = [UIColor.blue.cgColor,
-//                            UIColor.red.cgColor,
-//                            UIColor.green.cgColor,
-//                            UIColor.blue.cgColor]
-//        colorLayer.add(animation, forKey: nil)
-        let path = UIBezierPath.init()
-        path.move(to: CGPoint(x: 0, y: 150))
-        path.addCurve(to: CGPoint(x: 300, y: 150), controlPoint1: CGPoint(x: 75, y: 0), controlPoint2: CGPoint(x: 225, y: 300))
-        
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = path.cgPath
-        shapeLayer.strokeColor = UIColor.red.cgColor
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.lineWidth = 3.0
-        layerView.layer.addSublayer(shapeLayer)
-        
-        let shipLayer = CALayer()
-        shipLayer.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
-        shipLayer.position = CGPoint(x: 0, y: 150)
-        shipLayer.contents = UIImage(named: "ship")?.cgImage
-        shipLayer.contentsGravity = CALayerContentsGravity.resizeAspect
-        layerView.layer.addSublayer(shipLayer)
-        
         let animation = CAKeyframeAnimation()
-        animation.keyPath = "position"
-        animation.path = path.cgPath
+        animation.keyPath = "backgroundColor"
         animation.duration = 4.0
-        animation.rotationMode = CAAnimationRotationMode.rotateAuto
-        shipLayer.add(animation, forKey: nil)
+        // 需要开始和结束的关键帧来匹配当前属性的值
+        animation.values = [UIColor.blue.cgColor,
+                            UIColor.red.cgColor,
+                            UIColor.green.cgColor,
+                            UIColor.blue.cgColor]
+        // 缓冲函数
+        let funcs = CAMediaTimingFunction.init(name: .easeIn)
+        animation.timingFunctions = [funcs, funcs, funcs]
+        colorLayer.add(animation, forKey: nil)
+        //        let path = UIBezierPath.init()
+        //        path.move(to: CGPoint(x: 0, y: 150))
+        //        path.addCurve(to: CGPoint(x: 300, y: 150), controlPoint1: CGPoint(x: 75, y: 0), controlPoint2: CGPoint(x: 225, y: 300))
+        //
+        //        let shapeLayer = CAShapeLayer()
+        //        shapeLayer.path = path.cgPath
+        //        shapeLayer.strokeColor = UIColor.red.cgColor
+        //        shapeLayer.fillColor = UIColor.clear.cgColor
+        //        shapeLayer.lineWidth = 3.0
+        //        layerView.layer.addSublayer(shapeLayer)
+        //
+        //        let shipLayer = CALayer()
+        //        shipLayer.frame = CGRect(x: 0, y: 0, width: 64, height: 64)
+        //        shipLayer.position = CGPoint(x: 0, y: 150)
+        //        shipLayer.contents = UIImage(named: "ship")?.cgImage
+        //        shipLayer.contentsGravity = CALayerContentsGravity.resizeAspect
+        //        layerView.layer.addSublayer(shipLayer)
+        //
+        //        let animation = CAKeyframeAnimation()
+        //        animation.keyPath = "position"
+        //        animation.path = path.cgPath
+        //        animation.duration = 4.0
+        //        animation.rotationMode = CAAnimationRotationMode.rotateAuto
+        //        shipLayer.add(animation, forKey: nil)
     }
     
     func setUp() {
@@ -164,16 +176,16 @@ class AnimationsViewController: UIViewController {
     
     func setUpCATransition() {
         // 自定义
-//        let transition = CATransition()
-//        transition.type = CATransitionType.reveal
-//        transition.duration = 2.0
-//        imgView.layer.add(transition, forKey: nil)
-//
-//        let currentImg = imgView.image
-//        var index = imgs.firstIndex(of: currentImg) ?? 0
-//        index = (index + 1) % imgs.count
-//        let img = imgs[index]
-//        imgView.image = img
+        //        let transition = CATransition()
+        //        transition.type = CATransitionType.reveal
+        //        transition.duration = 2.0
+        //        imgView.layer.add(transition, forKey: nil)
+        //
+        //        let currentImg = imgView.image
+        //        var index = imgs.firstIndex(of: currentImg) ?? 0
+        //        index = (index + 1) % imgs.count
+        //        let img = imgs[index]
+        //        imgView.image = img
         // UIView
         UIView.transition(with: imgView, duration: 2.0, options: .transitionFlipFromLeft, animations: {
             let currentImg = self.imgView.image
@@ -185,7 +197,7 @@ class AnimationsViewController: UIViewController {
             
         }
     }
-
+    
     func customTransition() {
         UIGraphicsBeginImageContextWithOptions(layerView.bounds.size, true, 0.0)
         layerView.layer.render(in: UIGraphicsGetCurrentContext()!)
@@ -210,6 +222,67 @@ class AnimationsViewController: UIViewController {
         }
     }
     
+    func drawMediaFunction() {
+        for item in mediaTimeNames {
+            var cP1: [Float] = [0.0, 0.0]
+            var cP2: [Float] = [0.0, 0.0]
+            let mediaTimingFunc = CAMediaTimingFunction.init(name: item)
+            mediaTimingFunc.getControlPoint(at: 1, values: &cP1)
+            mediaTimingFunc.getControlPoint(at: 2, values: &cP2)
+            
+            let path = UIBezierPath()
+            path.move(to: CGPoint.zero)
+            let point1 = CGPoint(x: CGFloat(cP1[0]), y: CGFloat(cP1[1]))
+            let point2 = CGPoint(x: CGFloat(cP2[0]), y: CGFloat(cP2[1]))
+            path.addCurve(to: CGPoint(x: 1, y: 1), controlPoint1: point1, controlPoint2: point2)
+            
+            path.apply(CGAffineTransform.init(scaleX: 250, y: 250))
+            
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.path = path.cgPath
+            shapeLayer.strokeColor = UIColor.red.cgColor
+            shapeLayer.lineWidth = 3.0
+            shapeLayer.fillColor = UIColor.clear.cgColor
+            
+            layerView.layer.addSublayer(shapeLayer)
+            layerView.layer.isGeometryFlipped = true
+        }
+    }
+    
+    func ballJumps() {
+        ballImgView.center = CGPoint(x: 150, y: 32)
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position"
+        animation.duration = 2.0
+        animation.delegate = self
+        animation.values = [CGPoint(x: 150, y: 32),
+                            CGPoint(x: 150, y: 268),
+                            CGPoint(x: 150, y: 140),
+                            CGPoint(x: 150, y: 268),
+                            CGPoint(x: 150, y: 220),
+                            CGPoint(x: 150, y: 268),
+                            CGPoint(x: 150, y: 250),
+                            CGPoint(x: 150, y: 268)]
+        animation.timingFunctions = [CAMediaTimingFunction.init(name: .easeIn),
+                                     CAMediaTimingFunction.init(name: .easeOut),
+                                     CAMediaTimingFunction.init(name: .easeIn),
+                                     CAMediaTimingFunction.init(name: .easeOut),
+                                     CAMediaTimingFunction.init(name: .easeIn),
+                                     CAMediaTimingFunction.init(name: .easeOut),
+                                     CAMediaTimingFunction.init(name: .easeIn)]
+        
+        animation.keyTimes = [NSNumber(value: 0.0),
+                              NSNumber(value: 0.3),
+                              NSNumber(value: 0.5),
+                              NSNumber(value: 0.7),
+                              NSNumber(value: 0.8),
+                              NSNumber(value: 0.9),
+                              NSNumber(value: 0.95),
+                              NSNumber(value: 1.0)]
+        self.ballImgView.layer.position = CGPoint(x: 150, y: 268)
+        self.ballImgView.layer.add(animation, forKey: nil)
+    }
+    
 }
 
 extension AnimationsViewController: CAAnimationDelegate {
@@ -220,10 +293,10 @@ extension AnimationsViewController: CAAnimationDelegate {
     }
     
     func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-//        CATransaction.begin()
-//        CATransaction.setDisableActions(true)
-//        colorLayer.backgroundColor = ((anim as! CABasicAnimation).toValue as! CGColor)
-//        CATransaction.commit()
+        //        CATransaction.begin()
+        //        CATransaction.setDisableActions(true)
+        //        colorLayer.backgroundColor = ((anim as! CABasicAnimation).toValue as! CGColor)
+        //        CATransaction.commit()
         print("finised: \(false)")
     }
 }
